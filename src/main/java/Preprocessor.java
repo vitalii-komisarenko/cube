454f0e48324b694e1f6edecc4468b6c3d1a63b15
Vitalii Komisarenko <komisarenko.v@daikineurope.com>
2024-03-15 11:21:24 +0100

    Preprocessor: Line continuation: Fix regex

--- src/main/java/Preprocessor.java
+++ src/main/java/Preprocessor.java
@@ -1,6 +1,6 @@
 class Preprocessor {
     public static String processLineContinuation(String sourceCode) {
-        return sourceCode.replaceAll("\\\n", "");
+        return sourceCode.replaceAll("\\\\\\n", "");
     }
     public static String runPreprocessor(String sourceCode) {
         sourceCode = processLineContinuation(sourceCode);
