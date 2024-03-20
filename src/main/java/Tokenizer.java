e09e042f393ceb82398bd710351f93013ce076db
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2024-03-20 04:34:44 +0100

    Tokenizer: Add token types

---
+++ src/main/java/Tokenizer.java
@@ -0,0 +1,34 @@
+class Tokenizer {
+    enum TokenType {
+        MacroIdentifier,      // #include, #ifdef
+        Octothorp,            // #
+        DoubleOctothorp,      // ##
+        NewLine,              // \n
+
+        Indentifier,          // int, i, return etc.
+        Char,                 // 'a'
+        String,               // "string"
+        Integer,              // 123
+        Float,                // 1.23
+
+        OpenRoundBracket,     // (
+        ClosingRoundBracket,  // )
+        OpeningSquareBracket, // [
+        ClosingSquareBracket, // ]
+        OpeningCurlyBracket,  // {
+        ClosingCurlyBracket,  // }
+
+        Plus,                 // +
+        Minus,                // -
+        Asterisk,             // *
+        Slash,                // /
+        Ampersand,            // &
+        Comma,                // ,
+        Less,                 // <
+        LessOrEqual,          // <=
+        Assignment,           // =
+        IsEqual,              // ==
+        More,                 // >
+        MoreOrEqual,           // >=
+    }
+}
