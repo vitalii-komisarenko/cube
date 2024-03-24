0b3d179576214923854d70c6d7a2099cfb670d80
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2024-03-24 02:58:54 +0100

    Macro Processor: Read #define macros

--- src/main/java/MacroProcessor.java
+++ src/main/java/MacroProcessor.java
@@ -17,7 +17,19 @@ public class MacroProcessor {
         }
     }
 
-    public MacroProcessor(ArrayList<Token> tokens) {
+    public class MacroProcessorException extends Exception {
+        public MacroProcessorException() {
+            super("");
+        }
+    }
+
+    private void assertParserNotOutOfBounds() throws MacroProcessorException {
+        if (parser_pos >= tokens.size()) {
+            throw new MacroProcessorException();
+        }
+    }
+
+    public MacroProcessor(ArrayList<Token> tokens) throws MacroProcessorException {
         this.parser_pos = 0;
         this.tokens = tokens;
         this.macros_without_brackets = new HashMap<String, ArrayList<Token>>();
@@ -26,6 +38,73 @@ public class MacroProcessor {
 
         for (Map.Entry<String, ArrayList<Token>> entry : this.macros_without_brackets.entrySet()) {
             processMacroWithoutBrackets(entry.getKey(), entry.getValue(), 0);
+        }
+
+        try {
+            while(true) {
+                processOneMacro();
+            }
+        }
+        catch (AllMacrosProcessed e) {
+        }
+    }
+
+    void processOneMacro() throws MacroProcessorException, AllMacrosProcessed {
+        for (parser_pos = 0; parser_pos < tokens.size(); ++parser_pos) {
+            if (tokens.get(parser_pos).type == TokenType.MacroIdentifier) {
+                break;
+            }
+        }
+
+        if (parser_pos == tokens.size()) {
+            throw new AllMacrosProcessed();
+        }
+
+        String macro_type = tokens.get(parser_pos).value;
+
+        parser_pos++;
+        assertParserNotOutOfBounds();
+
+        if (tokens.get(parser_pos).type != TokenType.Identifier) {
+            throw new MacroProcessorException();
+        }
+        String macro_name = tokens.get(parser_pos).value;
+
+        parser_pos++;
+
+        ArrayList<Token> macro_paramaters = new ArrayList<Token>(); // including opening and closing brackets
+        if (tokens.get(parser_pos).type == TokenType.OpenRoundBracket) {
+            for (; parser_pos < tokens.size(); ++parser_pos) {
+                assertParserNotOutOfBounds();
+                Token token = tokens.get(parser_pos);
+                macro_paramaters.add(token);
+                if (token.type == TokenType.ClosingRoundBracket) {
+                    break;
+                }
+                if (token.type == TokenType.NewLine) {
+                    throw new MacroProcessorException();
+                }
+            }
+
+            assertParserNotOutOfBounds();
+            parser_pos++;
+        }
+
+        ArrayList<Token> macro_replacement = new ArrayList<Token>();
+        for (; parser_pos < tokens.size(); ++parser_pos) {
+            Token token = tokens.get(parser_pos);
+            if (token.type == TokenType.NewLine) {
+                break;
+            }
+            macro_replacement.add(token);
+        }
+        ++parser_pos;
+
+        if (macro_type == "#define") {
+            processMacroWithoutBrackets(macro_name, macro_replacement, parser_pos);
+        }
+        else {
+            throw new MacroProcessorException();
         }
     }
 
