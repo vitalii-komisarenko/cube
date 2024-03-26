815566bb137c36c829cb58ad2135a5b2a9f421b2
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2024-03-26 14:44:20 +0100

    Preprocessor: Convert line termination to Unix-style

--- src/main/java/Preprocessor.java
+++ src/main/java/Preprocessor.java
@@ -1,8 +1,12 @@
 class Preprocessor {
+    public static String convertLineTerminationsToUnix(String sourceCode) {
+        return sourceCode.replaceAll("\\\\\\r", "");
+    }
     public static String processLineContinuation(String sourceCode) {
         return sourceCode.replaceAll("\\\\\\n", "");
     }
     public static String runPreprocessor(String sourceCode) {
+        sourceCode = convertLineTerminationsToUnix(sourceCode);
         sourceCode = processLineContinuation(sourceCode);
         return sourceCode;
     }
